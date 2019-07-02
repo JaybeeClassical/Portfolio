148691d4fdc3ec9dde466f4a7d65b81a98b8adc4
JaybeeGeek <johnboscoobiejesi@gmail.com>
2019-07-02 08:40:57 +0100

    adding homepage to help navigation

--- src/App.js
+++ src/App.js
@@ -11,14 +11,16 @@ function App() {
       <Layout>
         <Header className="header-nav" title="Jude Chinweike Obiejesi" scroll>
           <Navigation>
-            <Link to="/resume">Resume</Link>
+            <Link to="/home">Home</Link>
             <Link to="/aboutme">About me</Link>
+            <Link to="/resume">Resume</Link>
             <Link to="/project">Project</Link>
             <Link to="/contact">Contact</Link>
           </Navigation>
         </Header>
         <Drawer title="I'm Jude">
           <Navigation>
+            <Link to="/home">Home</Link>
             <Link to="/resume">Resume</Link>
             <Link to="/aboutme">About me</Link>
             <Link to="/project">Project</Link>
--- src/components/main.js
+++ src/components/main.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import LandingPage from './landingpage';
 import { Switch, Route } from 'react-router-dom';
-import Resume from './resume';
+import Home from './landingpage';
 import Aboutme from './aboutme';
+import Resume from './resume';
 import Project from './project';
 import Contact from './contact';
 
 const Main = () => (
     <Switch>
         <Route exact path="/" component={LandingPage} />
-        <Route path="/resume" component={Resume} />
+        <Route path="/home" component={Home} />
         <Route path="/aboutme" component={Aboutme} />
+        <Route path="/resume" component={Resume} />
         <Route path="/project" component={Project} />
         <Route path="/contact" component={Contact} />
+
 
     </Switch>
 )
